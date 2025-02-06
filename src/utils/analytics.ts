ad49da88aeab14bc579f2731d348d87476a3d60f
Carmen Dominguez <carmengdominguez94@gmail.com>
2025-02-06 11:48:54 +0200

    chore: consoles

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -12,18 +12,21 @@ declare global {
 // Custom events
 export const analytics = {
   pageViewed: (page: string) => {
+    console.log('Attempting to track page view:', page);
     window.gtag?.('event', 'page_view', {
       page_title: page
     });
   },
 
   dateSelected: (date: string) => {
+    console.log('Attempting to track date selection:', date);
     window.gtag?.('event', 'select_date', {
       date_selected: date
     });
   },
 
   compareClicked: (astroSign: string, traditionalSign: string) => {
+    console.log('Attempting to track comparison:', { astroSign, traditionalSign });
     window.gtag?.('event', 'compare_signs', {
       astronomical_sign: astroSign,
       traditional_sign: traditionalSign
@@ -31,12 +34,14 @@ export const analytics = {
   },
 
   aboutOpened: () => {
+    console.log('Attempting to track about view');
     window.gtag?.('event', 'view_about', {
       event: 'view_about'
     });
   },
 
   emailSent: (success: boolean) => {
+    console.log('Attempting to track email send:', success);
     window.gtag?.('event', 'send_email', {
       success: success
     });
